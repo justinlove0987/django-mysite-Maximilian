abefe09b2ea1698fe601a64672a9e7092fdc1dc7
justinlove0987 <justinlove0987@gmail.com>
2021-09-29 14:51:53 +0800

    121 first commit

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,17 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
 
-class Post(models.Models):
+class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
@@ -21,3 +25,4 @@ class Post(models.Models):
     slug = models.SlugField(unique=True, db_index=True)
     content = TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
