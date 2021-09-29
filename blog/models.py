30388388e9e65889b95bc185146cb7e90ffc3db0
justinlove0987 <justinlove0987@gmail.com>
2021-09-29 14:41:39 +0800

    119 first commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.aggregates import Max
+from django.db.models.fields import TextField
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Post(models.Models):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    # db_index let Django and SQL trade a index for that field to make querying and filtering based on it a bit more effective.
+    # db_index=True is default.
+    slug = models.SlugField(unique=True, db_index=True)
+    content = TextField(validators=[MinLengthValidator(10)])
