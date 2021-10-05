e2d105f00937e0e9d164919d20d8b06949698d50
justinlove0987 <justinlove0987@gmail.com>
2021-10-05 14:16:50 +0800

    188 convert views to class based

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DeleteView
 
 from .models import Post
 
 
-# Create your views here.
+class StartingPageView(ListView):
+      template_name = "blog/index.html"
+      model = Post
+      context_object_name = "posts"
+      ordering = ["-date"]
+
+      def get_queryset(self):
+          queryset =  super().get_queryset()
+          data = queryset[:3]
+          return data
 
 
-def starting_page(request):
-    # -date means order by descending order, to make sure the post we added the last is on top.
-    # Django will convert this all statement into SQL command. 'Post.objects.all().order_by("-date")[:3]'
-    # 'Post.objects.all().order_by("-date")[-3:]' Django does not support negative index here!!!
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-      "posts": latest_posts
-    })
+class AllPostsView(ListView):
+      template_name = "blog/all-posts.html"
+      model = Post
+      ordering = ["-date"]
+      context_object_name = "all_posts"
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-      "all_posts": all_posts
-    })
+class SinglePostView(DeleteView):
+      template_name = "blog/post-detail.html"
+      model = Post
+      context_object_name = "post"
 
-
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-      "post": identified_post,
-      "post_tags": identified_post.tags.all()
-    })
+      def get_context_data(self, **kwargs):
+          cotext = super().get_context_data(**kwargs)
+          cotext["post_tags"] = self.object.tags.all()
+          return cotext
