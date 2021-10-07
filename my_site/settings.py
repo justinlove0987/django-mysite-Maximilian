2b6698125fcd60516cae4da6a0d9d9f0c81999dc
justinlove0987 <justinlove0987@gmail.com>
2021-10-07 10:54:20 +0800

    208 get started and revisiting settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# the secret key is used internally by django -
+# - for asigning cookies and for security aspects in gerneral.
 SECRET_KEY = 'django-insecure-s$^tz@jpm!c*zbbqs8&*eo%-)hhdl4fo$g3oh*n$6dj-l!sk20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
